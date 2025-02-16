ad7728dc865722a8148a114bf85d9456269d7404
martenzoe <biz.zoellner@gmail.com>
2025-02-16 17:51:11 +0100

    Added SQLiteDataManager Class, created the engine and Sessionmaker which are connected to the Database

---
+++ datamanager/sqlite_data_manager.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine  # Zum Erstellen einer Datenbankverbindung
+from sqlalchemy.orm import sessionmaker  # Zum Erstellen von Sitzungen (Sessions)
+from datamanager.data_manager_interface import DataManagerInterface  # Importiert das Interface
+
+class SQLiteDataManager(DataManagerInterface):  # Implementiert das Interface
+    def __init__(self, db_file_name): #Dateiname muss noch ergänzt werden
+        self.engine = create_engine(f'sqlite:///{db_file_name}')  # Erstellt eine Verbindung zur SQLite-Datenbank
+        Session = sessionmaker(bind=self.engine)  # Erstellt einen Sessionmaker
+        self.session = Session()  # Erstellt eine Sitzung
+
+
