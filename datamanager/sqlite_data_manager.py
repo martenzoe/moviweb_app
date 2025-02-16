10d7a274b36be6b131282f0ecb37ded17b5ab076
martenzoe <biz.zoellner@gmail.com>
2025-02-16 18:37:20 +0100

    Added the CRUD Operations in the sqlite_data_manager.py

--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -9,4 +9,38 @@ class SQLiteDataManager(DataManagerInterface):  # Implementiert das Interface
         Session = sessionmaker(bind=self.engine)  # Erstellt einen Sessionmaker
         self.session = Session()  # Erstellt eine Sitzung
 
+    def get_all_users(self):
+        return self.session.query(User).all()  # Gibt alle Benutzer zurück
 
+    def get_user_movies(self, user_id):
+        return self.session.query(Movie).filter_by(user_id=user_id).all()  # Gibt alle Filme eines Benutzers zurück
+
+    def add_user(self, name):
+        new_user = User(name=name)  # Erstellt einen neuen Benutzer
+        self.session.add(new_user)  # Fügt den Benutzer zur Sitzung hinzu
+        self.session.commit()  # Speichert die Änderungen in der Datenbank
+        return new_user
+
+    def add_movie(self, user_id, name, director, year, rating):
+        new_movie = Movie(user_id=user_id, name=name,
+                          director=director, year=year,
+                          rating=rating)
+        self.session.add(new_movie)
+        self.session.commit()
+        return new_movie
+
+    def update_movie(self, movie_id, **kwargs):
+        movie = self.session.query(Movie).get(movie_id)  # Holt den Film aus der DB
+        if not movie:
+            return None
+        for key, value in kwargs.items():
+            setattr(movie, key, value)  # Aktualisiert Attribute des Films
+        self.session.commit()
+        return movie
+
+    def delete_movie(self, movie_id):
+        movie = self.session.query(Movie).get(movie_id)
+        if not movie:
+            return None
+        self.session.delete(movie)
+        self.session.commit()
